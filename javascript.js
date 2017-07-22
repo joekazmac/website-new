3bce9595013cd3c5766106e80f2162099cb696dd
joekazmac <jkazmac2020295@sjvhs.com>
2017-07-21 22:30:22 -0400

    ye
    yeet

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	
+	$("headers").click(function(){
+		if(headers.hasClass(header1)){
+			headers.removeClass
+		}
+		else {
+			headers.AddClass(header1)
+		}
+	})
 });
